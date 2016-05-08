78f773ea07efb2321f81cc95cf48b900e15825e2
polun <965076377@qq.com>
2016-05-08 12:14:06 +0800

    增加柱形统计图

--- js/script.js
+++ js/script.js
@@ -30,6 +30,12 @@ function toArray(data) {
     });
 }
 
+function baseChartInfo() {
+    return {
+        title: '网站访问分布统计数据'
+    };
+}
+
 function generateData(data) {
     var first = 10;
     var last = data.length - first;
@@ -42,24 +48,43 @@ function generateData(data) {
     _.each(otherData, function(item) {
         otherDataSum += item.times;
     });
-    var chartData = _.map(mainData, function(item) {
-        return [item.hostname, item.times];
-    });
-    if (otherDataSum > 0) {
-        chartData.push(['其它', otherDataSum]);
+    var pieData = [],
+        barData = {
+            categories: [],
+            SeriesData: []
+        };
+    var len = mainData.length;
+    for (var i = 0; i < len; i++) {
+        pieData.push([mainData[i].hostname, mainData[i].times]);
+        barData.categories.push(mainData[i].hostname);
+        barData.SeriesData.push(mainData[i].times);
     }
-    drawChart(chartData);
+
+    if (otherDataSum > 0) {
+        pieData.push(['其它', otherDataSum]);
+        barData.categories.push('其它');
+        barData.SeriesData.push(otherDataSum);
+    }
+
+    drawPie({
+        title: '饼图分布',
+        data: pieData
+    });
+    drawBar({
+        title: '柱形统计',
+        data: barData
+    });
 }
 
-function drawChart(data) {
-    $('#container').highcharts({
+function drawPie(option) {
+    $('#pie').highcharts({
         chart: {
             plotBackgroundColor: null,
             plotBorderWidth: null,
             plotShadow: false
         },
         title: {
-            text: '网站访问分布统计数据'
+            text: option.title
         },
         credits: {
             enabled: false
@@ -83,7 +108,48 @@ function drawChart(data) {
         series: [{
             type: 'pie',
             name: '访问分布',
-            data: data
+            data: option.data
+        }]
+    });
+}
+
+function drawBar(option) {
+    $('#bar').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: option.title
+        },
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            categories: option.data.categories
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '次数 (次)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y} 次</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: '访问次数',
+            data: option.data.SeriesData
         }]
     });
 }
